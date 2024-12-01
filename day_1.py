e03fe90057ff936970e51ccd1eb407bf9ec33a42
Nils <tools@nilssteinlein.de>
2024-12-02 00:13:23 +0100

    day1

--- day_1.py
+++ day_1.py
@@ -1,1 +1,11 @@
+import numpy as np
 import pandas as pd
+
+df = pd.read_csv('input_day_1.txt', sep='\s+', header=None)
+
+col_0 = np.sort(df[0].values)
+col_1 = np.sort(df[1].values)
+
+difference = col_0 - col_1
+
+print(np.sum(difference))
